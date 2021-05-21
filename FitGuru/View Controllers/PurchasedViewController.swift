be60d961415fdcfa1e82de1a4c9217dd206942c8
Jeevan Bastola <jeevanbastola986@gmail.com>
2021-05-20 22:14:13 -0400

    my plans added

--- FitGuru/View Controllers/PurchasedViewController.swift
+++ FitGuru/View Controllers/PurchasedViewController.swift
@@ -6,15 +6,55 @@
 //
 
 import UIKit
+import Parse
 
-class PurchasedViewController: UIViewController {
+class PurchasedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground {(posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
+        let post = posts[indexPath.row]
+        
+        let user = post["author"] as! PFUser
+        cell.fitguruLabel.text = user.username
+        
+        cell.workoutLabel.text = post["workout"] as? String
+        cell.dietLabel.text = post["diet"] as? String
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
